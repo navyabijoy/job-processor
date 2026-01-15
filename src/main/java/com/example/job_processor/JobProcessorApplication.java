d8e8ba25a3682953029414f914349dde838c6aed
Navya Bijoy <navyabijoy14@gmail.com>
2026-01-15 12:08:23 +0530

    Initial commit

---
+++ src/main/java/com/example/job_processor/JobProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.example.job_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/job_processor/JobProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.job_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
